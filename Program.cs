80bb1c76c16e88142d1709964bab809d21939c97
Sergo331 <proha331@gmail.com>
2023-01-28 21:30:25 +0300

    время в очереди
    время в очереди

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int humanInTyrn;
+            int timeOfReceipt = 10;
+            int waitingTimeChas;
+            int waitingTimeMin;
+
+            
+
+            Console.Write("Для того, что бы расчитать время ожидания в очереди, введите колличество человек перед вами: ");
+            humanInTyrn = Convert.ToInt32(Console.ReadLine());
+
+            waitingTimeChas = (humanInTyrn * timeOfReceipt) / 60;
+            waitingTimeMin = (humanInTyrn * timeOfReceipt) % 60;
+
+            Console.WriteLine("Вы должны отстоять в очереди: " +waitingTimeChas + " часа" + waitingTimeMin + " минут.");
+
+
+
+
+        }
+    }
+}
