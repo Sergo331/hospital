961f2a8ff6afbb63ae76b0fef31a39794e13b3c4
Sergo331 <proha331@gmail.com>
2023-01-29 00:18:28 +0300

    очередь
    очередь

--- Program.cs
+++ Program.cs
@@ -4,26 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace задание_6
+namespace задание_7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int money;
-            int priceCrystal = 10;
-            int crystalCount;
+            int humanInQueue;
+            int timeOfReceipt = 10;
+            int waitingTimeHour;
+            int waitingTimeMin;
+            int minInHour = 60;
 
-            Console.WriteLine("Добро пожаловать в магазин кристаллов. Цена 1 кристалла " + priceCrystal + " монет.");
-            Console.Write("Сколько у вас монет?:");
-            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Для того, что бы расчитать время ожидания в очереди, введите колличество человек перед вами: ");
+            humanInQueue = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Сколько кристаллов Вам нужно?:");
-            crystalCount = Convert.ToInt32(Console.ReadLine()); 
+            waitingTimeHour = (humanInQueue * timeOfReceipt) / minInHour;
+            waitingTimeMin = (humanInQueue * timeOfReceipt) % minInHour;
 
-            money -= priceCrystal * crystalCount;
+            Console.WriteLine("Вы должны отстоять в очереди: " +waitingTimeHour + " часа " + waitingTimeMin + " минут.");
 
-            Console.WriteLine("У вас в сумке: " + crystalCount + " кристаллов и " +money +" монет.");
+
+
+
         }
     }
 }
